1236d3353c7a653ec2d541770ce45467ca9f1c18
utkarsh-goyal1 <ukgoyal123@gmail.com>
2024-01-31 22:57:08 +0530

    User Model Added

---
+++ backend/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    phoneNumber: {
+        type: Number,
+    },
+    profilePicture: {
+        type: String,
+    },
+    bookedTickets: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Show'
+        },
+        {
+            seatNo: {
+                type: String,
+
+            }
+        }
+    ]
+})
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
