c78634b47378b440f096ecfc25ca02d1593a7d44
Shubham Saurav <ssaurav28502@gmail.com>
2024-02-03 16:03:40 +0530

    made changes in cinemas

--- frontend/src/Components/Cinemas/Cinemas.jsx
+++ frontend/src/Components/Cinemas/Cinemas.jsx
@@ -1,63 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import CinemaCard from './CinemaCard'
 
 export default function Cinemas() {
   return (
-    <>
-    <Link to="">
-    <div className="relative flex w-full max-w-[26rem] hover:bg-gray-200 flex-col rounded-xl bg-transparent bg-clip-border border-2 text-gray-700 shadow-none">
-    <div className="relative flex items-center gap-4 p-3 overflow-hidden text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
-        <img
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1480&amp;q=80"
-        alt="Tania Andrew"
-        className="relative inline-block h-[58px] w-[58px] !rounded-full  object-cover object-center" />
-        <div className="flex w-full flex-col gap-0.5">
-        <div className="flex items-center justify-between">
-            <h5
-            className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-            Tania Andrew
-            </h5>
-            <div className="flex items-center gap-0 5">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                className="w-5 h-5 text-yellow-700">
-                <path fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                className="w-5 h-5 text-yellow-700">
-                <path fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                className="w-5 h-5 text-yellow-700">
-                <path fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                className="w-5 h-5 text-yellow-700">
-                <path fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"></path>
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                className="w-5 h-5 text-yellow-700">
-                <path fill-rule="evenodd"
-                d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                clip-rule="evenodd"></path>
-            </svg>
-            </div>
-        </div>
-        <p className="block font-sans text-base antialiased font-light leading-relaxed text-blue-gray-900">
-            Frontend Lead @ Google
-        </p>
-        </div>
+    <div className="flex flex-row flex-wrap gap-8 justify-center mt-10 my-6 sm:mx-10 md:mx-20">
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
+    <CinemaCard cinemaname="MLX" movie="hello brother" />
     </div>
-    </div>
-    </Link>
-      
-    </>
   )
 }
