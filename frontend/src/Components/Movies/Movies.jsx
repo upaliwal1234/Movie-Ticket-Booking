7022bd938b701f1413ba3f7110c05d75a28a1615
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-02-07 23:39:05 +0530

    Movies Page update

--- frontend/src/Components/Movies/Movies.jsx
+++ frontend/src/Components/Movies/Movies.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react"
 import MovieCard from "./MovieCard"
-import { useEffect } from "react";
 import { tokenCheck } from '../../../helperToken.js';
 import { useNavigate } from "react-router-dom"; import axios from "axios"
 import baseURL from "../../DB"
@@ -32,8 +31,8 @@ function Movies() {
     return (
         <div className="flex flex-row flex-wrap gap-8 justify-center mt-10 my-6 sm:mx-10 md:mx-20">
             {data.map((item, index) => (
-                <Link to={`/movies`}>
-                    <MovieCard key={index}
+                <Link to={`/movies/${item.name}/${item._id}`} key={index}>
+                    <MovieCard
                         image={item.poster}
                         title={item.name}
                         language={item.language}
