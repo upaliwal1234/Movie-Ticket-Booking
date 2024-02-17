5d76682e29c893df4c7e23859885d0cf6e80d0b6
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-02-17 15:58:59 +0530

    CardSlider attached to backend and Card added

--- frontend/src/Components/CardSlider/CardSlider.jsx
+++ frontend/src/Components/CardSlider/CardSlider.jsx
@@ -6,18 +6,9 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/bundle';
 import { Keyboard, Navigation } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 // import 'swiper/swiper-bundle.min.css';
 
-
-const movieData = [
-  {
-    image: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OC43LzEwICA5My4zSyBWb3Rlcw%3D%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00304730-sqjdzkhcrh-portrait.jpg",
-    title: "Fighter",
-    language: "Hindi",
-    certificate: "UA"
-  },
-  // Add more movie data objects as needed
-];
 
 export default function CardSlider({ items }) {
 
@@ -63,20 +54,14 @@ export default function CardSlider({ items }) {
         >
           {items.map((item, index) => (
             <SwiperSlide key={index} >
-              <div>
-                <div className="relative h-[380px] w-[220px]  flex justify-center rounded-md">
-                  <img
-                    src={item.image}
-                    alt={item.title}
-                    className="h-full rounded-md"
-                  />
-                </div>
-                <div className="ml-[2.5rem] my-4">
-                  <h1 className=" my-2 text-xl font-bold text-gray-900">{item.title}</h1>
-                  <h1 className=" my-1 text-sm font-thin text-gray-700">{item.certificate}</h1>
-                  <h1 className=" my-1 text-sm font-thin text-gray-700">{item.language}</h1>
-                </div>
-              </div>
+              <Link to={`/movies/${item.name}/${item._id}`} key={index}>
+                <MovieCard
+                  image={item.poster}
+                  title={item.name}
+                  language={item.language}
+                  certificate={item.certificate}
+                />
+              </Link>
             </SwiperSlide>
           ))}
         </Swiper>
@@ -85,13 +70,3 @@ export default function CardSlider({ items }) {
     </>
   )
 }
-
-
-
-
-
-
-
-
-
-
