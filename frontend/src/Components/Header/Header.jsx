69dc2bb15f484af7c90654469e74f328623b9223
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-02-17 15:58:20 +0530

    Header warning fixed

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   const [open, setOpen] = useState(false);
   const menuRef = useRef();
 
- console.log(open);
+  // console.log(open);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     const token = localStorage.getItem('myToken');
@@ -37,7 +37,7 @@ function Header() {
   const handleProfile = () => {
     navigate('/profile');
   }
-  
+
 
   return (
     <div className="relative w-full bg-white">
@@ -94,22 +94,22 @@ function Header() {
           {isLoggedIn ? (
             <div className='flex'>
               <div className='relative' ref={menuRef}>
-              <button onClick={() => setOpen(prevOpen => !prevOpen)} >
-                <img className='rounded-full w-[2.5rem] h-[2.5rem] ' src={profilepic} alt="Profile" srcset="" />
-              </button>
-              {open && 
-              <div className='shadow-md z-30 rounded-md bg-white absolute top-[3rem] -right-[4rem] p-2 w-[10rem]'>
-                <ul>
-                  <Link to='/profile'>
-                  <li  onClick={() => setOpen(prevOpen => !prevOpen)} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer'>Profile</li>
-                  </Link>
-                  <li  onClick={() => setOpen(prevOpen => !prevOpen)} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer'>Setting</li>
-                  <li  onClick={() => setOpen(prevOpen => !prevOpen)} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer'>Booking History</li>
-                  <li  onClick={handleLogout} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer border-t-2'>Logout</li>
-                </ul>
+                <button onClick={() => setOpen(prevOpen => !prevOpen)} >
+                  <img className='rounded-full w-[2.5rem] h-[2.5rem] ' src={profilepic} alt="Profile" />
+                </button>
+                {open &&
+                  <div className='shadow-md z-30 rounded-md bg-white absolute top-[3rem] -right-[4rem] p-2 w-[10rem]'>
+                    <ul>
+                      <Link to='/profile'>
+                        <li onClick={() => setOpen(prevOpen => !prevOpen)} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer'>Profile</li>
+                      </Link>
+                      <li onClick={() => setOpen(prevOpen => !prevOpen)} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer'>Setting</li>
+                      <li onClick={() => setOpen(prevOpen => !prevOpen)} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer'>Booking History</li>
+                      <li onClick={handleLogout} className='p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer border-t-2'>Logout</li>
+                    </ul>
+                  </div>
+                }
               </div>
-              }
-            </div>
             </div>
           ) : (
             <>
