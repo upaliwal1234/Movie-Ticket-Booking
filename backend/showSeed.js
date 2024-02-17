f8ef72de0efe793c3b52b7b2a1aa588c07923753
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2024-02-17 16:33:01 +0530

    show db update

--- backend/showSeed.js
+++ backend/showSeed.js
@@ -8,7 +8,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/Movie-Booking-Site')
 let dummyData = [
   {
     timing: '11:00 AM',
-    date: '14-02-2024',
+    date: '17-02-2024',
+    movieName: 'Fighter',
+    price: 220,
+    seating: [false, false, false],
+    cinema: '65c3ca582bc86a356c0905c6'
+  },
+  {
+    timing: '03:00 PM',
+    date: '17-02-2024',
     movieName: 'Fighter',
     price: 220,
     seating: [false, false, false],
@@ -16,7 +24,7 @@ let dummyData = [
   },
   {
     timing: '02:00 PM',
-    date: '14-02-2024',
+    date: '18-02-2024',
     movieName: 'Fighter',
     price: 220,
     seating: [false, false, false],
@@ -24,61 +32,28 @@ let dummyData = [
   },
   {
     timing: '04:00 PM',
-    date: '14-02-2024',
+    date: '18-02-2024',
     movieName: 'Fighter',
     price: 220,
     seating: [false, false, false],
     cinema: '65c3ca582bc86a356c0905c8'
   },
   {
-    timing: '11:30 AM',
-    date: '14-02-2024',
-    movieName: 'The Dark Knight',
+    timing: '04:00 PM',
+    date: '19-02-2024',
+    movieName: 'Fighter',
     price: 220,
-    seating: [false, false, false]
+    seating: [false, false, false],
+    cinema: '65c3ca582bc86a356c0905c8'
   },
   {
-    timing: '01:30 PM',
-    date: '14-02-2024',
-    movieName: 'Inception',
+    timing: '06:00 PM',
+    date: '19-02-2024',
+    movieName: 'Fighter',
     price: 220,
-    seating: [false, false, false]
-  },
-  {
-    timing: '03:30 PM',
-    date: '14-02-2024',
-    movieName: 'Interstellar',
-    price: 220,
-    seating: [false, false, false]
-  },
-  {
-    timing: '12:00 PM',
-    date: '14-02-2024',
-    movieName: 'Jurassic Park',
-    price: 220,
-    seating: [false, false, false]
-  },
-  {
-    timing: '02:30 PM',
-    date: '14-02-2024',
-    movieName: 'Jaws',
-    price: 220,
-    seating: [false, false, false]
-  },
-  {
-    timing: '05:00 PM',
-    date: '14-02-2024',
-    movieName: 'Titanic',
-    price: 220,
-    seating: [false, false, false]
-  },
-  {
-    timing: '10:00 AM',
-    date: '14-02-2024',
-    movieName: 'The Shawshank Redemption',
-    price: 220,
-    seating: [false, false, false]
-  },
+    seating: [false, false, false],
+    cinema: '65c3ca582bc86a356c0905c8'
+  }
 ];
 
 async function seed() {
