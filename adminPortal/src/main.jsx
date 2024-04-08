6c21b3653aa29294d800c2f4b4920aaf5c4d62ae
Shubham Saurav <ssaurav28502@gmail.com>
2024-04-08 22:35:30 +0530

    routes

--- adminPortal/src/main.jsx
+++ adminPortal/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      
+    </Route>
+
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
