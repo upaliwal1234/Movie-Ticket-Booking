84debfedec394650f20727a866a5d44397f32b21
utkarsh-goyal1 <ukgoyal123@gmail.com>
2024-04-09 23:14:51 +0530

    searching

--- adminPortal/src/Pages/MoviePage.jsx
+++ adminPortal/src/Pages/MoviePage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
-function MoviePage(){
-  
+import { useState,useNavigate} from 'react';
+import axios from 'axios';
+import baseURL from '../'
+function MoviePage() {
+  const navigate=useNavigate();
+  const [suggestions, setSuggestions] = useState([]);
+  const [movieName, setMovieName] = useState('');
+  const handleChange = (e) => {
+    setMovieName(e.target.value);
+  }
   const movies = [
     {
       id: 1,
@@ -164,30 +171,55 @@ function MoviePage(){
         "https://image.tmdb.org/t/p/w500/nrsx0jEaBgXq4PWo7SooSnYJTv.jpg",
     },
   ];
-
+  const handleClick = async () => {
+    setSuggestions([]);
+    fetchdata();
+  }
+  const fetchdata = async () => {
+    try {
+      let response = await axios.get(`${baseURL}/admin/search/movie/${movieName}`);
+      // console.log(response.data);
+      if (response.status === 200) {
+        let data = response.data;
+        setMovieName('');
+        navigate(`/movies/${response.data.name}/${response.data._id}`);
+      }
+    }
+    catch (err) {
+      console.error('Error in fetching data', err);
+    }
+  }
+  const handleSuggestionClick = (movieName) => {
+    setMovieName(movieName);
+    setSuggestions([]);
+  }
   return (
     <div style={{ backgroundColor: 'rgb(245, 245, 245)' }}>
       <h1 className="text-3xl text-red-500 text-center m-2 font-bold">Movies</h1>
       <div className="flex justify-center items-center">
-  <input
-    className="border w-3/4 p-3 mt-7 mr-2"
-    type="text"
-    placeholder="Search Movie . . ."
-  />
-  <button className="h-12 w-42 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md mt-7">Add New Movie</button>
-</div>
+        <input
+          className="border w-3/4 p-3 mt-7 mr-2"
+          type="text"
+          value={movieName}
+          onChange={handleChange}
+          placeholder="Search Movie . . ."
+        />
+        <button className='m-1' type='submit' onClick={handleClick}>
+          <svg className='w-5 fill-gray-500 mr-1 hover:fill-black' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg>
+        </button>
+      </div>
 
       <div className="mt-16 mx-10">
-      <div className="card grid grid-cols-3 gap-10">
-        {movies.map((item) => (
-          <div className="text-center border rounded-md p-4" key={item.id}>
-            <img src={item.imageUrl} alt="" />
-            <h5 className="text-lg font-semibold">{item.title}</h5>
-            <h4 className="text-lg text-gray-500">{item.year}</h4>
-            <h4 className="text-lg text-gray-500">{item.director}</h4>
-          </div>
-        ))}
-      </div>
+        <div className="card grid grid-cols-3 gap-10">
+          {movies.map((item) => (
+            <div className="text-center border rounded-md p-4" key={item.id}>
+              <img src={item.imageUrl} alt="" />
+              <h5 className="text-lg font-semibold">{item.title}</h5>
+              <h4 className="text-lg text-gray-500">{item.year}</h4>
+              <h4 className="text-lg text-gray-500">{item.director}</h4>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
